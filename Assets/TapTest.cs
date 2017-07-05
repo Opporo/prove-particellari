36c8633ad299ebedd9d9ba9bb245f1b9eee0ea71
Irene Zanon <i.zanon@eggoemail.com>
2017-07-05 17:40:36 +0200

    Tap della carta -> da outline a cerchio (sistemare la transizione contraria)

---
+++ Assets/TapTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTest : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeState()
+    {
+        anim.SetInteger("cardState", 1);
+    }
+}
