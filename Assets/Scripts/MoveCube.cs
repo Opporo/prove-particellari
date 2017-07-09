e9cb8932f8dbeae059dc9854bba2b6a589dcd5f2
NYX-IRENE\Irene <meglioquesto@gmail.com>
2017-07-10 01:34:16 +0200

    Effetto per danno da fuoco, iTween per motion path attacchi

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+    private ParticleSystem particleSys;
+
+	// Use this for initialization
+	void Start () {
+        //MoveAlongPath();
+        particleSys = this.GetComponent<ParticleSystem>();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown("a")){
+            particleSys.Play();
+            MoveAlongPath();
+        }
+    }
+
+    public void MoveAlongPath()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("pat"), "time", 1.5, "easetype", iTween.EaseType.linear));
+    }
+}
